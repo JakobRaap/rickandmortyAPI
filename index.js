a80216c7d0d9d9ce1d24899a124b0cf8734c82ff
Jakob Raap <bokaj.paar@gmail.com>
2023-04-03 11:55:24 +0200

    next button and pagination

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
+let maxPage;
+let page = 1;
 const searchQuery = "";
 
-async function fetchCharacters() {
+async function fetchCharacters(page) {
+  cardContainer.innerHTML = ``;
   try {
-    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const response = await fetch(
+      "https://rickandmortyapi.com/api/character?page=" + page
+    );
 
     if (!response.ok) {
       console.error("response not okay");
@@ -27,17 +30,28 @@ async function fetchCharacters() {
     console.error("an error occurred");
   }
 }
-const apiData = await fetchCharacters();
-console.log(apiData);
-//source, name, status, type, occurrences
-apiData.results.forEach((element) => {
-  cardContainer.append(
-    createCharacterCard(
-      element.image,
-      element.name,
-      element.status,
-      element.type,
-      element.episode.length
-    )
-  );
+async function loadPage() {
+  const apiData = await fetchCharacters(page);
+  maxPage = apiData.info.pages;
+  console.log(apiData);
+  apiData.results.forEach((element) => {
+    cardContainer.append(
+      createCharacterCard(
+        element.image,
+        element.name,
+        element.status,
+        element.type,
+        element.episode.length
+      )
+    );
+  });
+  pagination.textContent = `${page}/${maxPage}`;
+}
+loadPage();
+
+nextButton.addEventListener("click", () => {
+  if (page < maxPage) {
+    page++;
+    loadPage();
+  }
 });
